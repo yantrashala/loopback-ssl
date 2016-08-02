62fecb5abfec5f5c2e291ec9b6f0fc23ea55bc02
Siddhartha Lahiri <slahiri@sapient.com>
2016-08-02 14:15:38 +0530

    Added test case frameworks for index and ssl

---
+++ test/loopback-ssl-test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('Testing SSL Server Initialization', function () {
+
+});
+
+describe('Testing HTTP Server Initialization', function () {
+
+});
+
+describe('Testing Certificate Configuration', function () {
+
+});
