6a18eabc44e3d3d29fcd0a1179c458e2273baabe
Siddhartha Lahiri <slahiri@sapient.com>
2016-08-02 18:17:42 +0530

    Added unit test cases

--- test/loopback-ssl-test.js
+++ test/loopback-ssl-test.js
@@ -1,16 +1,34 @@
 'use strict';
 
+var loopbackSSL = require('../lib/loopback-ssl');
+
 var chai = require('chai');
 var expect = chai.expect;
 
-describe('Testing SSL Server Initialization', function () {
-
-});
-
-describe('Testing HTTP Server Initialization', function () {
-
-});
-
 describe('Testing Certificate Configuration', function () {
+  it('test:1 - should be able to load server key and certificates', function(done){
+    done();
+  });
 
+  it('test:2 - should be able to load server key, ' +
+    'certificates and client authentication certificate', function(done){
+    done();
+  });
+
+  it('test:3 - should be able to load server key, ' +
+    'certificates and multiple client authentication certificate', function(done){
+    done();
+  });
+
+  it('test:4 - should be able enable mutual ssl', function(done){
+    done();
+  });
+
+  it('test:5 - should be able disable client certificate validation', function(done){
+    done();
+  });
+
+  it('test:6 - should fail if the key path is invalid', function(done){
+    done();
+  });
 });
