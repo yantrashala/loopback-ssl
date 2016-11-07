ad188cf3b8f5ed5a9e86a2c4b2f2436e45fa8eac
Siddhartha Lahiri <slahiri@sapient.com>
2016-11-07 21:44:05 +0530

    Added cert file test cases.

--- test/loopback-ssl-test.js
+++ test/loopback-ssl-test.js
@@ -3,15 +3,15 @@
 var loopbackSSL = require('../lib/loopback-ssl');
 var assert = require('chai').assert;
 
-//var testConfig1 = require('./fixtures/configs/config-1.json');
-//var testConfig2 = require('./fixtures/configs/config-2.json');
-//var testConfig3 = require('./fixtures/configs/config-3.json');
+var testConfig1 = require('./fixtures/configs/config-1.json');
+var testConfig2 = require('./fixtures/configs/config-2.json');
+var testConfig3 = require('./fixtures/configs/config-3.json');
 var testConfig4 = require('./fixtures/configs/config-4.json');
 var testConfig5 = require('./fixtures/configs/config-5.json');
 var testConfig6 = require('./fixtures/configs/config-6.json');
+var testConfig7 = require('./fixtures/configs/config-7.json');
 
 describe('Testing Certificate Configuration', function () {
-  /*
   it('test:1 - should be able to load server key and certificates', function(done){
     var ret = loopbackSSL.getServerOptions(testConfig1.certConfig);
     done();
@@ -28,7 +28,6 @@ describe('Testing Certificate Configuration', function () {
     var ret = loopbackSSL.getServerOptions(testConfig3.certConfig);
     done();
   })
-  */
 
   it('test:4 - should be able enable mutual ssl', function(done){
     var ret = loopbackSSL.getServerOptions(testConfig4.certConfig);
@@ -45,6 +44,14 @@ describe('Testing Certificate Configuration', function () {
   it('test:6 - should fail if the key path is invalid', function(done){
     try {
       loopbackSSL.getServerOptions(testConfig6.certConfig);
+    } catch (e) {
+      done();
+    }
+  });
+
+  it('test:7 - should pass httpMode=true and no other options are provided', function(done){
+    try {
+      loopbackSSL.getServerOptions(testConfig7.certConfig);
     } catch (e) {
       done();
     }
