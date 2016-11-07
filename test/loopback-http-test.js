f8c7174da9ca6255bd9895ed30fcc5186676b6f7
Siddhartha Lahiri <slahiri@sapient.com>
2016-11-08 00:22:56 +0530

    Added description for http and https test cases.

--- test/loopback-http-test.js
+++ test/loopback-http-test.js
@@ -4,12 +4,14 @@ var lt = require('loopback-testing');
 var assert = require('assert');
 var app = require('./fixtures/server/server.js'); //path to app.js or server.js
 
-describe('/notes', function() {
-  lt.beforeEach.withApp(app);
-  lt.describe.whenCalledRemotely('GET', '/notes', function() {
-    it('should have statusCode 200', function(done) {
-      assert.equal(this.res.statusCode, 200);
-      done();
+describe('Testing HTTP Server Start', function () {
+  describe('/notes', function() {
+    lt.beforeEach.withApp(app);
+    lt.describe.whenCalledRemotely('GET', '/notes', function() {
+      it('should have statusCode 200', function(done) {
+        assert.equal(this.res.statusCode, 200);
+        done();
+      });
     });
   });
 });
--- test/loopback-https-test.js
+++ test/loopback-https-test.js
@@ -4,12 +4,14 @@ var lt = require('loopback-testing');
 var assert = require('assert');
 var app = require('./fixtures/server-ssl/server.js'); //path to app.js or server.js
 
-describe('/notes', function() {
-  lt.beforeEach.withApp(app);
-  lt.describe.whenCalledRemotely('GET', '/notes', function() {
-    it('should have statusCode 200', function(done) {
-      assert.equal(this.res.statusCode, 200);
-      done();
+describe('Testing HTTPS Server Start', function () {
+  describe('/notes', function() {
+    lt.beforeEach.withApp(app);
+    lt.describe.whenCalledRemotely('GET', '/notes', function() {
+      it('should have statusCode 200', function(done) {
+        assert.equal(this.res.statusCode, 200);
+        done();
+      });
     });
   });
 });
